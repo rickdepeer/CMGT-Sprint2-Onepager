a2a55dbddeea6877184c97beaec5d63f46fcf30c
Laptop\rick_ <rickdepeer@gmail.com>
2017-10-03 14:08:11 +0200

    Contact for gemaakt en style verbeteringetjes

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rick_
+ * Date: 03/10/2017
+ * Time: 12:44
+ */
+
+
+/*
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
--- js/ready.js
+++ js/ready.js
@@ -2,4 +2,10 @@ $(document).ready(function()
 {
     $('#fullpage').fullpage();
 
+    $(document.body).on("click", ".nav-link", function ()
+    {
+        $(".nav-link").removeClass("active");
+        $(this).addClass("active");
+    })
+
 });
